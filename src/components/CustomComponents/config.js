03eeca7bc48dfe2a75d42f27d964fad25da13699
eethaldevteam <eethalnaditsolutions@gmail.com>
2025-11-20 10:02:17 +0530

    config added

--- src/components/CustomComponents/config.js
+++ src/components/CustomComponents/config.js
@@ -2,8 +2,7 @@
 
 
 export const config = {
-  Api: 'http://127.0.0.1:8001/api/',                                           //Local server
-  // Api:'https://enis-hrm-node.onrender.com',                                  //Snab server
-  // Api:'https://eethal-hrm-node.onrender.com'                                 // Eethal server
+  // Api: 'http://127.0.0.1:8001/api/',                                           //Local server
+  Api:'https://plot-management-node.onrender.com/api/',                                  //Snab server
 };
 
